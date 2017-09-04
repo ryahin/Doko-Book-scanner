4a3fadb12869b30461d79bca1c15043a7954019c
Василий <naukomania@gmail.com>
2017-09-04 17:34:30 +0300

    minor edits

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 100)
+            scrollTop: ($($anchor.attr('href')).offset().top - 70)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -10,11 +10,14 @@ $(document).ready(function() {
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
-        offset: 101
+        offset: 71
     });
     $(window).paroller({
         factor: 0.6,            // multiplier for scrolling speed and offset
         type: 'background',     // background, foreground
         direction: 'vertical' // vertical, horizontal, TODO: diagonal
+    });
+    $('.modal-close').click(function(){
+        $('#popup-form').modal('toggle');
     });
 });
