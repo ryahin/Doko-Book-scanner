37ace6b0f351c1bc97e3efcd2e37389d72b7fd93
Василий <naukomania@gmail.com>
2017-09-07 18:56:53 +0300

    validate and send message

--- js/script.js
+++ js/script.js
@@ -13,21 +13,21 @@ $(document).ready(function() {
         offset: 71
     });
     $(window).paroller({
-        factor: 0.6,            // multiplier for scrolling speed and offset
-        type: 'background',     // background, foreground
+        factor: 0.6, // multiplier for scrolling speed and offset
+        type: 'background', // background, foreground
         direction: 'vertical' // vertical, horizontal, TODO: diagonal
     });
-    $('.modal-close').click(function(){
-        $('#popup-form').modal('toggle'); 
+    $('.modal-close').click(function() {
+        $('#popup-form').modal('toggle');
     });
-    $('.buy-modal-close').click(function(){
-        $('#buy-form').modal('toggle'); 
+    $('.buy-modal-close').click(function() {
+        $('#buy-form').modal('toggle');
     });
     var bookSpreadOffsetTop = $('#book-spread').offset().top;
     var bookSpreadHeight = $('#book-spread').height();
-    var viewPortHeight = $( window ).height();
-    var topScroll = bookSpreadOffsetTop - viewPortHeight/2;
-    var bottomScroll = bookSpreadOffsetTop + viewPortHeight/2;
+    var viewPortHeight = $(window).height();
+    var topScroll = bookSpreadOffsetTop - viewPortHeight / 2;
+    var bottomScroll = bookSpreadOffsetTop + viewPortHeight / 2;
     $(document).scroll(function(event) {
         if ($(document).scrollTop() > topScroll && $(document).scrollTop() < bottomScroll) {
             $('#book-spread').addClass('show-animation');
@@ -40,8 +40,54 @@ $(document).ready(function() {
         var selectedModel = $(this).attr('data-model');
         $('#model').val(selectedModel);
     });
-    $('.politics').click(function(e){
+    $('.politics').click(function(e) {
         e.preventDefault();
         $('.politics-info').fadeToggle();
+    });
+    // $('#button-send-by').click(function(e){
+    //     e.preventDefault();
+    // });
+    $("#buy-selected-model").validate({
+        rules: {
+            user_email: {
+                required: true,
+                email: true
+            },
+            model: {
+                required: true
+            }
+        },
+        // выводимые сообщения при нарушении соответствующих правил
+        messages: {
+            "user_email": {
+                required: "Необходимо указать почтовый ящик",
+                email: "Пожалуйста, укажите корректный почтовый ящик"
+            },
+            "model": {
+                required: "Необходимо выбрать модель сканера"
+            }
+        },
+
+        // указаваем обработчик
+        submitHandler: function(form) {
+            $.post(
+                'submit.php',
+                $(form).serialize(),
+                function(data) {
+                    if (data.message) {
+                        $('#form-message').html(data.message);
+                    }
+                    if (data.success) {
+                        $('#form-message').removeClass('alert-danger');
+                        $('#form-message').addClass('alert-success');
+                        $(form).find('input').prop('disabled', true);
+                        $(form).find('textarea').prop('disabled', true);
+                    } else {
+                        $('#form-message').removeClass('alert-success');
+                        $('#form-message').addClass('alert-danger');
+                    }
+                }
+            );
+        }
     });
 });
---
+++ submit.php
@@ -0,0 +1,49 @@
+<?php
+$data = array(
+	'success' => true,
+	'message' => 'Ваша заявка успешно отправлена!',
+);
+
+$email 		= $_POST['user_email'] 		? strip_tags($_POST['user_email']) : '';
+$modelId 		= $_POST['model'] 		? strip_tags($_POST['model']) : '';
+$message	= $_POST['order_body']	? strip_tags($_POST['order_body']) : '';
+
+$models = array('DOKO ET16','DOKO BS16');
+$model = isset($models[$modelId-1]) ? $models[$modelId-1] : '';
+
+if (!$email) {
+	$data = array(
+		'success' => false,
+		'message' => 'Необходимо указать почтовый ящик',
+	);
+} else {
+	$body = '<html><body>';
+	$body .= '<h1>Заявка на сайте DOKO!</h1>';
+	$body .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+	if ($email)
+		$body .= "<tr><td><strong>e-mail:</strong> </td><td>" . htmlentities($email) . "</td></tr>";
+	if ($model)
+		$body .= "<tr><td><strong>модель сканера:</strong> </td><td>" . htmlentities($model) . "</td></tr>";
+	if ($message)
+		$body .= "<tr><td><strong>сообщение:</strong> </td><td>" . htmlentities($message) . "</td></tr>";
+	$body .= "</table>";
+	$body .= '</body></html>';
+
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+	$headers .= 'From: <info@doc-cam.ru>' . "\r\n";
+
+	if (!mail (
+		'info@doc-cam.ru',
+		'Заявка на сайте DOKO',
+		$body,
+		$headers
+	)) {
+		$data = array(
+			'success' => false,
+			'message' => 'Не удалось отправить заявку. Пожалуйста, попробуйте позднее',
+		);
+	}
+}
+header('Content-Type: application/json');
+echo json_encode($data);
